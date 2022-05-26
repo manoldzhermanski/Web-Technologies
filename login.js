81b46a01320b5acdc2d0221850aa9e41313dead0
Veliana Kirova <veliana.kirova2@gmail.com>
2022-05-26 17:55:05 +0300

    Add data validation for register and login

--- login.js
+++ login.js
@@ -3,19 +3,15 @@ const onSubmit = () => {
     const username = document.getElementById("username");
     const password = document.getElementById("password");
     
-    let currUser = username.value.trim();
-    let currPassword = password.value.trim();
+    let userInput = username.value.trim();
+    let passwordInput = password.value.trim();
 
     for (const [key, value] of Object.entries(localStorage)) {
-        if(key === "user"){
-            let text = localStorage.getItem("user");
-            let user = JSON.parse(text);
-            console.log(user.username)
-            console.log(user.password);
-
-            if(currUser === user.username) {
-                if (currPassword === user.password){
-                    location.href='personal-blog.html';
+        if(key.includes("user")){
+            let user = JSON.parse(localStorage.getItem(key));
+            if(user.username === userInput) {
+                if (passwordInput === user.password){
+                    location.href='index.html';
                 } else {
                     toggleModal(true, "Invalid password.");
                 }
@@ -25,6 +21,36 @@ const onSubmit = () => {
         }
     }
 }
+
+function saveUsersFromMongoDbToLocalStorage() {
+    fetch('http://localhost:3002/users')
+        .then((response) => response.json())
+        .then((users) => {users
+            if (users && users.length !== 0) {
+                users.map(user => {
+                if (!isLocalStorageAlreadyContainingUser(user.username)){
+                    let rand = Math.random() * 1000;
+                    let uniqueKey = "user" + Math.floor(rand);
+                    localStorage.setItem(uniqueKey, JSON.stringify(user));  
+                }
+                })
+            }
+        })
+}
+
+const isLocalStorageAlreadyContainingUser = (username) => {
+    for (const key of Object.keys(localStorage)) {
+        if(key.includes("user")){
+            let user = JSON.parse(localStorage.getItem(key));
+            if(user.username === username){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+saveUsersFromMongoDbToLocalStorage();
 
 const toggleModal = (open, msg) => {
     let dialogBackground = document.getElementById("dialog-background");
